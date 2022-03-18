dfabe1e32137118d1c40cc8cad26afe29180b1cd
Anuj Jamwal <anuj.jamwal1@gmail.com>
2022-03-18 01:15:45 -0400

    Moved common redis code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate core;
 
+#[cfg(feature = "proto")]
 pub mod grpc;
 
 pub mod id;
@@ -10,6 +11,11 @@ pub mod proto;
 #[cfg(feature = "longrunning")]
 pub mod longrunning;
 
+#[cfg(feature = "proto")]
 pub mod prost;
 
+#[cfg(feature = "proto")]
 pub mod kube;
+
+#[cfg(feature = "redis")]
+pub mod redis;
